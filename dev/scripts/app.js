e5b17d7b706200a24e949cef2bb779bc1279c8da
Chantale Barnard <barnardchantale@gmail.com>
2017-09-06 16:34:08 -0400

    Update app.js

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('react!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { 
+    BrowserRouter as Router, 
+    Route, Link } from 'react-router-dom';
+import { ajax } from 'jquery';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>React!</h1>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
